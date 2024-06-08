e8816dbfa45ce5cb4a0a7c1ae391e09f798e999f
Tay Kong Yu <kongyutay@gmail.com>
2024-06-08 15:29:03 +0800

    Done routes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Header from "./common/Header";
 import Home from "./pages/Home";
 import NotFoundPage from "./pages/NotFoundPage";
 import SinglePage from "./pages/SinglePage";
+import Footer from "./common/Footer";
 
 function App() {
   return (
     <>
       <Router>
         <Header />
+
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/singleProduct" element={<SinglePage />} />
           <Route path="/*" element={<NotFoundPage />} />
         </Routes>
+
+        <Footer />
       </Router>
     </>
     
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Trending from "../components/Trending";
+import TrendingGrid from "../components/TrendingGrid";
+import PopularPost from "../components/PopularPost";
+import News from "../components/News";
+import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Trending />
+            <TrendingGrid />
+            <PopularPost />
+            <News />
+            <NewsLetter />
+        </>
     )
 }
 
